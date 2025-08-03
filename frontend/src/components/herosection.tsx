05df1bf695f8eff13e1998330c277ba48747a5d4
shadow-coder24 <shadowcoder24@gmail.com>
2025-08-03 19:46:20 +0530

    commit

--- frontend/src/components/herosection.tsx
+++ frontend/src/components/herosection.tsx
@@ -1,18 +1,37 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import Image from "next/image";
 
-function HeroSection() {
+const images = [
+  "/banner/banner1.png",
+  "/banner/banner2.png",
+  "/banner/banner3.png",
+  "/banner/banner4.png",
+  "/banner/banner5.png",
+  "/banner/banner6.png",
+  "/banner/banner7.png",
+];
+
+export default function HeroSection() {
   return (
-    <div>
-        <Image 
-            src="/banner/banner1.png"
-            alt="Hero Banner"
-            width={1550}
-            height={100}
-            className="w-full h-screen object-cover"
-        />
-    </div>
-  )
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{ delay: 3000 }}
+      navigation={true}
+      modules={[Autoplay, Navigation]}
+    >
+      {images.map((src, i) => (
+        <SwiperSlide key={i}>
+          <div className="relative w-screen h-[650px]">
+            <Image src={src} alt={`Banner ${i + 1}`} fill className="object-cover" />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
-
-export default HeroSection
